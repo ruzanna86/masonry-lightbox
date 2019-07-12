1ade286c4a4cd4730c1471c5bfef8ccb04d27683
ruzanna86 <ruzanna.mehrab@gmail.com>
2019-07-12 19:09:43 +0400

    Added massonry functionality in script

--- js/masonry.js
+++ js/masonry.js
@@ -13,7 +13,52 @@
 
     $.fn.masonry = function (options) {
         this.options = $.extend({}, defaults, options);
-        console.log(this);
+        var that = this;
+        var topPosition = 0;
+        var leftPosition = 0;
+        var oneColHeight = [];
+        $(this).css('width', $(window).width());
+
+        this.columnCount = Math.floor(($(window).width() / parseInt(this.options.width)));
+        $(this.children()).css({
+            'width': this.options.width,
+            'padding': this.options.padding
+        });
+
+        this.updateItem = function(i){
+            $(that[0].children[i]).css({
+                'left': leftPosition,
+                'top': topPosition
+            });
+        };
+        for(var j = 0; j < this.columnCount; j++){
+            oneColHeight.push(parseInt($(this[0].children[j]).outerHeight(true)));
+            if(j == 0){
+                leftPosition = 0;
+            }else{
+                leftPosition += parseInt(this.options.width);
+            }
+            topPosition = 0;
+            this.updateItem(j);
+        }
+
+        for(var i = 0; i <  this.children().length; i++){
+            if(i >= this.columnCount){
+                var minHeight = oneColHeight[0];
+                for(var j = 0; j < oneColHeight.length; j++){
+                    if(oneColHeight[j] < minHeight){
+                        minHeight = oneColHeight[j];
+                    }
+                }
+                var currentIndex = oneColHeight.indexOf(minHeight);
+                leftPosition = currentIndex * parseInt(this.options.width);
+                topPosition = minHeight;
+                oneColHeight[currentIndex] = minHeight + parseInt($(this[0].children[i]).outerHeight(true) );
+                this.updateItem(i);
+
+            }
+        }
+
     };
 
 })(jQuery);
